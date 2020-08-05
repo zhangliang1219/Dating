161e6a3e65dac0f33673db362f634d1953059b09
Karan Suthar <karanstridhyatech@gmail.com>
2020-08-05 15:50:33 +0530

    Search page and Header Started

--- resources/views/front/profile/index.blade.php
+++ resources/views/front/profile/index.blade.php
@@ -4,12 +4,50 @@
     $preferred_age = config('constant.preferred_age');
 @endphp
 @section('content')
+<div class="page-header-title">
+    <div class="header-background"></div>
+    <div class="inner-header">
+        <div class="container">
+            <div class="title-section">
+                <h2>Profile</h2>
+                <div class="breadcrumbs">
+                    <ul>
+                        <li><a href="">Home ></a></li>
+                        <li>Profile</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="profile-header">
+        <div class="profile-header-image">
+            <div class="image" style="background-image:url('{{ asset('css/images/profile-default.jpg')}}')">
+                <div class="edit-btn">
+
+                </div>
+            </div>
+            
+        </div>
+        <div class="profile-details">
+            <div class="profile-user">
+                <div class="user-image">
+                    <img src="{{ asset('css/images/profile-default.jpg')}}" alt="">
+                </div>
+                <div class="user-details">
+                    <h3>Sarika Parmar</h3>
+                    <h5>33 year old Woman</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header"></div>
-
                 <div class="card-body">
                     @if ($errors->any())
                         <div class="alert alert-danger">
