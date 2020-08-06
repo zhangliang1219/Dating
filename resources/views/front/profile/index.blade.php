4c5e87d62e4e89c53c8675f2d39128aab9cc21ea
Karan Suthar <karanstridhyatech@gmail.com>
2020-08-06 18:36:11 +0530

    Profile

--- resources/views/front/profile/index.blade.php
+++ resources/views/front/profile/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.final')
 @php
-    $gender = trans('sentence.gender');
-    $preferred_age = config('constant.preferred_age');
-    $height = config('constant.height');
-    $weight = config('constant.weight');
+$gender = trans('sentence.gender');
+$preferred_age = config('constant.preferred_age');
+$height = config('constant.height');
+$weight = config('constant.weight');
 @endphp
 @section('content')
 <div class="page-header-title">
@@ -24,22 +24,100 @@
 </div>
 <div class="container">
     <div class="profile-header">
-        <div class="profile-header-image">
-            <div class="image" style="background-image:url('{{ asset('css/images/profile-default.jpg')}}')">
-                <div class="edit-btn">
-
-                </div>
+        <div class="profile-header-image" style="background-image:url('{{ asset('images/profile-default.jpg')}}')">
+            <div class="edit-btn">
+                <ion-icon name="create-outline"></ion-icon>
             </div>
-            
+            <div class="social-media">
+                <a class="facebook" href="">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a class="twitter" href="">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a class="linkedin" href="">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+            </div>
         </div>
         <div class="profile-details">
             <div class="profile-user">
                 <div class="user-image">
-                    <img src="{{ asset('css/images/profile-default.jpg')}}" alt="">
+                    <img src="{{ asset('images/profile-default.jpg')}}" alt="">
+                    <div class="edit-btn">Edit</div>
                 </div>
                 <div class="user-details">
                     <h3>Sarika Parmar</h3>
                     <h5>33 year old Woman</h5>
+                    <h6><i class="fas fa-map-marker-alt"></i> Tokyo, Japan</h6>
+                </div>
+            </div>
+            <div class="likes">
+                <h3>121</h3>
+                <h6>Number of Likes</h6>
+            </div>
+        </div>
+        <div class="images-upload">
+            <div class="profile-header-title">
+                <h3>About Upload Photo</h3>
+                <button>
+                    <ion-icon name="create-outline"></ion-icon>
+                </button>
+            </div>
+            <div class="images-slider">
+                <div class="swiper-container swiper-container-1">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="slider-image">
+                                <img src="{{ asset('images/profile/pexels-rafael-barros-1996887.jpg') }}" alt="">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -52,61 +130,62 @@
                 <div class="card-header"></div>
                 <div class="card-body">
                     @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @endif
                     @if (session()->has('success'))
-                        <div class="alert alert-success" role="alert">
-                            <h5>{{ session('success') }}</h5>
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        <h5>{{ session('success') }}</h5>
+                    </div>
                     @endif
-                    <form class="form-horizontal" method="post" action="{{route('viewSearchProfile')}}" name="search_profile" id="search_profile" >
+                    <form class="form-horizontal" method="post" action="{{route('viewSearchProfile')}}"
+                        name="search_profile" id="search_profile">
                         @csrf
                         <div class="card-body">
                             <div class="form-group col-4">
-                                    <select name="gender" class="form-control">
-                                        <option value="">{{ trans('sentence.gender_label')}}</option>
-                                        @foreach($gender as $key => $val)
-                                            <option value="{{$key}}">{{$val}}</option>
-                                        @endforeach
-                                    </select>
+                                <select name="gender" class="form-control">
+                                    <option value="">{{ trans('sentence.gender_label')}}</option>
+                                    @foreach($gender as $key => $val)
+                                    <option value="{{$key}}">{{$val}}</option>
+                                    @endforeach
+                                </select>
                             </div>
                             <div class="form-group col-4">
                                 <select name="age" class="form-control">
                                     <option value="">{{ trans('sentence.age')}}</option>
                                     @foreach($preferred_age as $key => $val)
-                                        <option value="{{$key}}">{{$val}}</option>
+                                    <option value="{{$key}}">{{$val}}</option>
                                     @endforeach
-                                </select> 
+                                </select>
                             </div>
                             <div class="form-group col-4">
-                                <select name="country" class="form-control" >
+                                <select name="country" class="form-control">
                                     <option value="">{{ trans('sentence.country')}}</option>
                                     @foreach($country as $key => $val)
-                                        <option value="{{$val->id}}">{{$val->country_name}}</option>
+                                    <option value="{{$val->id}}">{{$val->country_name}}</option>
                                     @endforeach
-                                </select> 
+                                </select>
                             </div>
                             <div class="form-group col-4">
-                                <select name="height" class="form-control" >
+                                <select name="height" class="form-control">
                                     <option value="">{{ trans('sentence.height')}}</option>
                                     @foreach($height as $key => $val)
-                                        <option value="{{$key}}">{{$val}}</option>
+                                    <option value="{{$key}}">{{$val}}</option>
                                     @endforeach
-                                </select> 
+                                </select>
                             </div>
                             <div class="form-group col-4">
-                                <select name="weight" class="form-control" >
+                                <select name="weight" class="form-control">
                                     <option value="">{{ trans('sentence.weight')}}</option>
                                     @foreach($weight as $key => $val)
-                                        <option value="{{$key}}">{{$val}}</option>
+                                    <option value="{{$key}}">{{$val}}</option>
                                     @endforeach
-                                </select> 
+                                </select>
                             </div>
                         </div>
                         <div class="form-group row mb-0">
